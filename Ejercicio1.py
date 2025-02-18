c0575d81f870317bd68673c8ab72943dc4286b58
Octavio <up240204@alumnos.upa.edu.mx>
2025-02-18 08:34:53 -0600

    programa 4

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,1 +1,6 @@
 age = 18
+
+base = float(input("Ingresa el valor de la base: "))
+altura = float(input("Ingresa el valor de la altura: "))
+area = 0.5 * base * altura
+print("El área es de:",area,"unidades cuadradas")
