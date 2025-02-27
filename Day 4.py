2da9b744eb74e4e6bfc3599336dc5b988298ad02
Octavio <up240204@alumnos.upa.edu.mx>
2025-02-26 20:23:37 -0600

    abrazos, no balazos

---
+++ Day 4.py
@@ -0,0 +1,8 @@
+#1. Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+word_1 = 'Thirty'
+word_2 = 'Days'
+word_3 = 'Of'
+word_4 = 'Python'
+space = ' '
+full_sentence = word_1 + space + word_2 + space + word_3 + space + word_4
+print(full_sentence)
