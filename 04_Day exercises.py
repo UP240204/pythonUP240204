95a6a7822b9ae9362008857b5d6bd6dd9b5eda03
Octavio <up240204@alumnos.upa.edu.mx>
2025-02-27 11:52:19 -0600

    ahi va quedando

--- 04_Day exercises.py
+++ 04_Day exercises.py
@@ -6,3 +6,20 @@ word_4 = 'Python'
 space = ' '
 full_sentence = word_1 + space + word_2 + space + word_3 + space + word_4
 print(full_sentence)
+
+#2. Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+word_5 = 'Coding'
+word_6 = 'For'
+word_7 = 'All'
+fulll_sentence = word_5 + space + word_6 + space + word_7
+print(fulll_sentence)
+
+#3. Declare a variable named company and assign it to an initial value "Coding For All".
+company = 'Coding For All'
+
+#4. Print the variable company using print().
+print(company)
+
+#5. Print the length of the company string using len() method and print().
+lenght = len(company)
+print("La longitud de 'Coding For All' es de",lenght)
