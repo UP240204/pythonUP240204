74b6cf754ecbd03b2daaa66a2863897a397ede9a
Octavio <up240204@alumnos.upa.edu.mx>
2025-03-12 09:33:39 -0600

    arre con la que barre

--- 06_Day exercises.py
+++ 06_Day exercises.py
@@ -37,3 +37,17 @@ food_stuff_lt = list(food_stuff_tp)
 print(food_stuff_lt)
 
 #4. Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+print(food_stuff_lt[:4] + food_stuff_lt[5:])
+
+#5. Slice out the first three items and the last three items from food_staff_lt list.
+print(food_stuff_lt[3:6])
+
+#6. Delete the food_staff_tp tuple completely.
+del food_stuff_lt
+
+#7. Check if an item exists in tuple:
+#-Check if 'Estonia' is a nordic country
+#-Check if 'Iceland' is a nordic country
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print("¿Estonia es un pais nordico?",'Estonia' in nordic_countries)
+print("¿Islandia es un pais nordico?",'Iceland' in nordic_countries)
