f1ece73a298db8dfdbd385a28fddbea2e0f87fac
Octavio <up240204@alumnos.upa.edu.mx>
2025-03-19 10:38:29 -0600

    estamo al 💯 y subiendo

---
+++ 08_Day/01_Exercises.py
@@ -0,0 +1,26 @@
+#1. Create an empty dictionary called dog.
+dog = {}
+
+#2. Add name, color, breed, legs, age to the dog dictionary.
+dog['Nombre'] = 'Dante'
+dog['Color'] = 'Café'
+dog['Raza'] = 'Pastor Aleman'
+dog['Patas'] = '4'
+dog['Edad en años'] = '5'
+print(dog)
+
+#3. Create a student dictionary and add first_name, last_name, gender, age, marital status, skills, country, city and address as keys for the dictionary.
+student = {
+    'first_name':'Octavio',
+    'last_name':'Hernández',
+    'gender':'Masculino',
+    'age':'18',
+    'is_marred':False,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'country':'México',
+    'city':'Aguascalientes',
+    'address':{
+        'street':'Robinia',
+        'zipcode':'20326'
+        }
+}
