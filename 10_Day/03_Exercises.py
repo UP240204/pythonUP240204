883451aad2e529ca62b22a1f57939040fe29ddd0
Octavio <up240204@alumnos.upa.edu.mx>
2025-03-28 23:34:48 -0600

    aqui andamos que es lo bueno

--- 10_Day/03_Exercises.py
+++ 10_Day/03_Exercises.py
@@ -14,3 +14,26 @@ for fruit in reversed(fruits):
 #-What are the total number of languages in the data.
 #-Find the ten most spoken languages from the data.
 #-Find the 10 most populated countries in the world.
+import countries_data as a
+datos = a.caca
+country_language = []
+for pais in datos:
+    for lenguaje in pais['languages']:
+        country_language.append(lenguaje)
+country_language_set = set(country_language)
+print("Hay un total de",len(country_language_set),"lenguajes:",country_language_set)
+
+dictlanguages = {}
+for language in country_language_set:
+    dictlanguages[language] = 0
+print(dictlanguages)
+for idioma in dictlanguages:
+    for pais in datos:
+        if idioma in pais['languages']:
+            dictlanguages[idioma] = pais['population'] + dictlanguages[idioma]
+sort_keys_languages_population = sorted(dictlanguages.values(),reverse=True)
+sort_values_languages_population = sorted(dictlanguages,key=dictlanguages.get,reverse=True)
+print(sort_keys_languages_population[1],sort_values_languages_population[1])
+
+for c in range(10):
+    print(sort_keys_languages_population[c],sort_values_languages_population[c])
