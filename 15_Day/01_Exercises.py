fabf5969641ddab491d1efbd7d97ab02ac6b153b
Octavio <up240204@alumnos.upa.edu.mx>
2025-04-03 20:36:57 -0600

    uyuyuy profe, ahora si, pa donde sea, menos pa la casa 😮‍💨

---
+++ 15_Day/01_Exercises.py
@@ -0,0 +1,25 @@
+#1. Open you python interactive shell and try all the examples covered in this section.
+#------------------------------ERRORES DE PROGRAMACION EN PYTHON 😮‍💨----------------------------------------------
+#                                   SYNTAX ERROR
+print 'andan jugando bien mal mis chivas'
+#                                   NAME ERROR
+print(fuga_al_gym)
+#                                   INDEX ERROR
+numbers = [1,2,3,4]
+print(numbers[5])
+#                                   MODULE NOT FOUND ERROR
+import cheves
+#                                   ATTRIBUTE ERROR
+import math
+math.PI
+#                                   KEY ERROR
+pepitos = {'hijos': 20, 'hijas':5}
+print(pepitos['morros'])
+#                                   TYPE ERROR
+print('saca un' + 6 + 'de cheves')
+#                                   IMPORT ERROR
+from math import power
+#                                   VALUE ERROR
+int('MTR01')
+#                                   ZERO DIVISION ERROR
+division = 1/0
